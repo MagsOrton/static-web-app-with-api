a104d5e7dc907dee1fd628126675c3f76be20c44
Margaret Orton <maorton@microsoft.com>
2021-06-18 12:55:45 +0100

    api commit

--- api/getPostcodeLookup/index.js
+++ api/getPostcodeLookup/index.js
@@ -1,12 +1,10 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
+    const postcode = (req.query.postcode || (req.body && req.body.postcode));
+    const responseMessage = postcode;
+     
+    context.res.json = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
